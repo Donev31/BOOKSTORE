c130f07f8da97bc0a708b731a7121bc01cf17e34
Едис Донев <edis.donev@abv.bg>
2024-02-02 21:46:33 +0200

    class

---
+++ BookStore/Controllers/AuthorController.cs
@@ -0,0 +1,41 @@
+﻿using BookStore.BL.Interfaces;
+using BookStore.Models.Models.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IAuthorService _authorService;
+        public AuthorController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        [HttpGet("GetAll")]
+        public List<Author> GetAll()
+        {
+            return _authorService.GetAll();
+        }
+
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("Id..");
+            }
+
+            var response = _authorService.GetById(id);
+
+            if (response == null)
+            {
+                return NotFound(id);
+            }
+
+            return Ok(_authorService.GetById(id));
+        }
+    }
+}
